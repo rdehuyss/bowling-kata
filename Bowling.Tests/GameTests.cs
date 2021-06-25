5c350a7efec0d365325f62c91d2042cff6b1f839
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2021-06-25 14:23:44 +0200

    Initial commit

---
+++ Bowling.Tests/GameTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bowling.Tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void RollGutter()
+        {
+            Game game = new Game();
+        }
+    }
+}
