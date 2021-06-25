9d085cc786fbb82f1f68dae655461ed584dfd456
lte <lte@bringme.com>
2021-06-25 16:25:41 +0200

    Refactoring Game

---
+++ Bowling/Frame.cs
@@ -0,0 +1,25 @@
+﻿namespace Bowling
+{
+    internal class Frame
+    {
+        public int? FirstRoll { get; set; }
+        public int? SecondRoll { get; set; }
+
+        public void Roll(int score)
+        {
+            if (this.FirstRoll == null)
+            {
+                this.FirstRoll = score;
+            }
+            else
+            {
+                this.SecondRoll = score;
+            }
+        }
+
+        public bool isCompleted()
+        {
+            return this.FirstRoll != null && this.SecondRoll != null;
+        }
+    }
+}
--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -1,22 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bowling
 {
     public class Game
     {
         private int score;
-        private int frames;
+
+        private int framesCount;
+        private List<Frame> frames = new List<Frame>();
+        private Frame currentFrame = new Frame();
 
         public void Roll(int pinsDown)
         {
             this.score += pinsDown;
+
+            currentFrame.Roll(pinsDown);
+
+            if (currentFrame.isCompleted())
+            {
+                this.frames.Add(currentFrame);
+                this.currentFrame = new Frame();
+            }
             
-            frames++;
+            framesCount++;
         }
 
         public int Score()
         {
-            if (frames > 20)
+            if (framesCount > 20)
             {
                 throw new InvalidOperationException();
             }
