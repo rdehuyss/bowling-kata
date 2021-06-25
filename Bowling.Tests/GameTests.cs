91d06d3d077d7b6289bc9775ecc87a4b2fcee56a
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2021-06-25 15:18:22 +0200

    WIP Finish rollGutter and roll one pin down

--- Bowling.Tests/GameTests.cs
+++ Bowling.Tests/GameTests.cs
@@ -5,10 +5,26 @@ namespace Bowling.Tests
 {
     public class GameTests
     {
+        private Game game = new Game();
+        
         [Fact]
         public void RollGutter()
         {
-            Game game = new Game();
+            RollFrame(0, 0);
+            Assert.Equal(0, game.Score());
+        }
+        
+        [Fact]
+        public void RollOnePin()
+        {
+            RollFrame(1, 0);
+            Assert.Equal(1, game.Score());
+        }
+
+        private void RollFrame(int firstRoll, int secondRoll)
+        {
+            game.Roll(firstRoll);
+            game.Roll(secondRoll);
         }
     }
 }
--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -4,5 +4,16 @@ namespace Bowling
 {
     public class Game
     {
+        private int score;
+
+        public void Roll(int pinsDown)
+        {
+            this.score += pinsDown;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
     }
 }
